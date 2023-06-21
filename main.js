83bc3e6a04922b71aab0652dbb6edc2952036914
PlatonKynin <kininplaton@gmail.com>
2023-06-21 20:17:58 +0300

    squareDigits

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Найти сумму чисел
 // function findSum(numbers) {
 //   let sum = 0;
-
+  
 //   if (numbers.length === 0) return 0;
 //   for (let i = 0; i < numbers.length; i++) {
 //     sum += numbers[i];
@@ -34,7 +34,7 @@
 
 // 2ой способ через map
 function fakeBin(x) {
-  return x.split('').map(n => n > 5 ? 1 : 0).join('')
+   return x.split('').map(n => n > 5 ? 1 : 0).join('')
 }
 // console.log('45385593107843568'.split('')[0]);
 // console.log(''.concat('0'));
@@ -50,39 +50,14 @@ function fakeBin(x) {
 */
 
 function century(year) {
-
+  
   return Math.ceil(year / 100);
 }
 // console.log(century(1601)); // 17
 
 
 function disemvowel(str) {
-  return str.split('').filter(el => !('aeiouAEIOU').includes(el)).join('');
+  return str.split('').filter(el => !('aeiou').includes(el)).join('');
 }
 
-// console.log(disemvowel("This website is for losers LOL!")); // Ths wbst s fr lsrs LL!
-
-
-// Digit*Digit
-function squareDigits(num) {
-  return +Array.from(String(num)).map(num => num ** 2).join('');
-}
-
-// console.log(squareDigits(765)); // 49-36-25 (493625)
-
-
-//Counting sheep...
-function countSheeps(arrayOfSheep) {
-  return arrayOfSheep.join('').replace(/false/gi, '').length/4;
-  // return arrayOfSheep.filter(el => el).length; // 17
-  // return arrayOfSheep.filter(Boolean).length; // 17
-};
-
-const arrayOfSheep = [true,  true,  true,  false,
-  true,  true,  true,  true ,
-  true,  false, true,  false,
-  true,  false, false, true ,
-  true,  true,  true,  true ,
-  false, false, true,  true]
-
-console.log(countSheeps(arrayOfSheep)); // количечество true - 17 
+console.log(disemvowel("This website is for losers LOL!"));
